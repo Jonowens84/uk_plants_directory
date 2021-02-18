e8c2e83fa9e5d48bebe228781e4c45f12ea94cc6
Jon Owens <jonowens84@gmail.com>
2021-02-18 09:30:31 +0000

    plant-data file created for clear background png. images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import classicModernScheme from './plants';
+import classicModernScheme from './plant-data/plants';
+
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        
-        <h1>{classicModernScheme.map
-          (
-            ({ name }) => `\n${name} `
-          )
-          .join("")}</h1>
+      <h1>A modern planting scheme</h1> 
       </header>
+      
+        <list>{classicModernScheme.map
+          (
+            ({ name }) => `\n${name},  `
+          )
+          .join("")}</list>
+    
     </div>
   );
 }
